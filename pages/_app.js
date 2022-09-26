bc19199cb3d2a6a32a7561faadf7e684f8211cf8
CyPhEr420 <vimalvk9900@gmail.com>
2022-09-26 18:11:51 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,8 @@ import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
-function MyApp({ Component, pageProps }) {
+function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
